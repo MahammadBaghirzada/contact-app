e0a97149b9f58e7e5b4755397ccd0eddc905e2a6
Mahammad <mbagirzade.01@gmail.com>
2023-02-01 12:19:00 +0400

    Named routes

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html = "
+        <h1>Contact App</h1>
+        <div>
+            <a href='" . route('contacts.index') . "'>All contacts</a>
+            <a href='" . route('contacts.create') . "'>Add contact</a>
+            <a href='" . route('contacts.show', 1) . "'>Show contact</a>
+        </div>
+    ";
+    return $html;
 });
 
 Route::get('contacts', function () {
     return "<h1>All contacts</h1>";
-});
+})->name('contacts.index');
 
 Route::get('/contacts/create', function () {
     return "<h1>All new contacts</h1>";
-});
+})->name('contacts.create');
 
 Route::get('/contacts/{id}', function ($id) {
     return "Contact " . $id;
-})->whereNumber('id');//->where('id', '[0-9]+');
+})->name('contacts.show');
 
-Route::get('/companies/{name?}', function ($name = null) {
-    if ($name) {
-        return "Company " . $name;
-    } else {
-        return "All Companies";
-    }
-})->whereAlphaNumeric('name');//->whereAlpha('name');//->where('name', '[a-zA-Z]+');
+//Route::get('/contacts/{id}', function ($id) {
+//    return "Contact " . $id;
+//})->whereNumber('id');//->where('id', '[0-9]+');
+//
+//Route::get('/companies/{name?}', function ($name = null) {
+//    if ($name) {
+//        return "Company " . $name;
+//    } else {
+//        return "All Companies";
+//    }
+//})->whereAlphaNumeric('name');//->whereAlpha('name');//->where('name', '[a-zA-Z]+');

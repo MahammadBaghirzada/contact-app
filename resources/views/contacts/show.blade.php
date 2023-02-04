9aeb65612ce15350ebd3449b08474af82fa90e13
Mahammad <mbagirzade.01@gmail.com>
2023-02-04 12:30:00 +0400

    Another approach passing data to the views

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -7,13 +7,13 @@
     <title>Contact App</title>
 </head>
 <body>
-    <h1>All contacts</h1>
+    <h1>Contact details</h1>
     <div>
-        <a href='{{ route('contacts.create') }}'>Add contact</a>
-
-        @foreach($contacts as $id => $contact)
-            <p>{{ $contact['name'] }} | {{ $contact['phone'] }} | <a href="{{ route('contacts.show', $id) }}">Show</a></p>
-        @endforeach
+        <p>Name: {{ $contact['name'] }}</p>
+        <p>Phone: {{ $contact['phone'] }}</p>
+    </div>
+    <div>
+        <a href='{{ route('contacts.index') }}'>Back to all contacts</a>
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+function getContacts() {
+    return [
+        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
+        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+    ];
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/contacts', function () {
-    $contacts = [
-        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
-    ];
+    $contacts = getContacts();
     return view('contacts.index', compact('contacts'));
 })->name('contacts.index');
 
@@ -31,9 +35,9 @@ Route::get('/contacts/create', function () {
 })->name('contacts.create');
 
 Route::get('/contacts/{id}', function ($id) {
-    return "Contact " . $id;
+    $contacts = getContacts();
+    //abort_if(!isset($contacts[$id]), 404);
+    abort_unless(isset($contacts[$id]), 404);
+    $contact = $contacts[$id];
+    return view('contacts.show')->with('contact', $contact);
 })->name('contacts.show');
-
-Route::fallback(function () {
-    return "<h1>Sorry, the page does not exist</h1>";
-});

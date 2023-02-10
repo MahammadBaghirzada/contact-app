df2363a6a8f8ed0e5ca603f94acddaffbdc81f5b
Mahammad <mbagirzade.01@gmail.com>
2023-02-10 15:46:33 +0400

    Dependency Injection in the Controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function index()
+    public function __construct(protected CompanyRepository $company)
     {
-        $companies = [
-            1 => ['name' => 'Company One', 'contacts' => '3'],
-            2 => ['name' => 'Company Two', 'contacts' => '5'],
-        ];
+    }
+
+    public function index(CompanyRepository $company)
+    {
+        $companies = $company->pluck();
         $contacts = $this->getContacts();
         return view('contacts.index', compact('contacts', 'companies'));
     }
